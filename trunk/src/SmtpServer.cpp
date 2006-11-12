266fce648e7acdd08f18b0b199c6d4b9474a3163
webb.clint <webb.clint@161fd20a-9a19-0410-9e87-6d96dc193b1e>
2006-11-12 23:37:05 +0000

    All functionality will be in the same process, using multiple threads.  Not complete

---
+++ trunk/src/SmtpServer.cpp
@@ -0,0 +1,49 @@
+//-----------------------------------------------------------------------------
+// Mailsrvd
+//  (c) Copyright Hyper-Active Systems, 2006.
+//
+// Hyper-Active Systems
+// ABN: 98 400 498 123
+// 66 Brown Cr
+// Seville Grove, WA, 6112
+// Australia
+// Phone: +61 434 895 695
+//        0434 895 695
+// contact@hyper-active.com.au
+// 
+//-----------------------------------------------------------------------------
+
+#include "SmtpServer.h"
+#include "Message.h"
+
+//-----------------------------------------------------------------------------
+SmtpServer::SmtpServer() 
+{	
+	_log.SetName("SmtpServer");
+	_log.Log("SmtpServer()");
+}
+		
+//-----------------------------------------------------------------------------
+SmtpServer::~SmtpServer() 
+{
+	_log.Log("~SmtpServer()");
+}
+
+
+
+//-----------------------------------------------------------------------------
+// CJW: A new connection has arrived.  We need to create an object to handle 
+// 		it, and add it to the list of objects.
+void SmtpServer::OnAccept(SOCKET nSocket)
+{
+	Message *pMsg;
+	
+	ASSERT(nSocket > 0);
+	
+	_log.Log("Connection received on socket %d.", nSocket);
+	pMsg = new Message; 
+	ASSERT(pMsg != NULL);
+	pMsg->Accept(nSocket);
+	AddSession(pMsg);
+}
+
---
+++ trunk/src/SmtpServer.h
@@ -0,0 +1,45 @@
+//-----------------------------------------------------------------------------
+// Mailsrvd
+//  (c) Copyright Hyper-Active Systems, 2006.
+//
+// Hyper-Active Systems
+// ABN: 98 400 498 123
+// 66 Brown Cr
+// Seville Grove, WA, 6112
+// Australia
+// Phone: +61 434 895 695
+//        0434 895 695
+// contact@hyper-active.com.au
+// 
+//-----------------------------------------------------------------------------
+
+#ifndef __SMTPSERVER_H
+#define __SMTPSERVER_H
+
+
+#include <stdio.h>
+#include <string.h>
+
+#include <DevPlus.h>
+
+#include "Server.h"
+
+
+class SmtpServer : public Server
+{
+	private:
+		
+	public:
+		SmtpServer(); 
+		virtual ~SmtpServer();
+		
+	protected:
+		virtual void OnAccept(SOCKET nSocket);
+		
+	private:
+		
+};
+
+
+
+#endif
